be16796061632c8e0cb5349f238f30bb0b2ad703
EniolaCodes <adebimpeniola@gmail.com>
2026-03-05 15:22:27 +0100

    feat: add footer component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,18 +7,18 @@ import CoreValue from "@/modules/landing/components/CoreValue/CoreValue";
 import Testimonial from "@/modules/landing/components/Testimonial/Testimonial";
 import FreeTrial from "@/modules/landing/components/FreeTrial/FreeTrial";
 import Footer from "@/modules/landing/components/Footer/Footer";
+import BackToTop from "./BackToTop";
 
 export const metadata: Metadata = {
-  title: "EduTrac – LMS",
+  title: "EduTrac",
   description:
     "EduTrac is a comprehensive learning management system designed to revolutionize education through technology.",
 };
 
 export default function HomePage() {
-  // CONFIGURATION OBJECT: Central place to edit text
   const content = {
     hero: {
-      title: "Revolutionizing Education Management",
+      title: "Revolutionizing Learning Management",
       description:
         "EduTrac was born from a vision to revolutionize education through technology.We understand the challenges schools face in managing operations, engaging students, and empowering educators. Our mission is to provide a comprehensive, user-friendly platform that streamlines school management, enhances communication, and fosters a collaborative learning environment. With EduTrac, we aim to transform the educational experience for administrators, teachers, students, and parents alike.",
       buttonText: "Get Started",
@@ -52,6 +52,7 @@ export default function HomePage() {
       /> */}
 
       <Footer />
+      <BackToTop />
     </Box>
   );
 }
