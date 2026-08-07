d79ef55103e2d40f40a7415a55697ffb7337b1a6
MobuchiElly <ugwueleazer@gmail.com>
2026-08-07 02:09:17 +0100

    refactor(email service): switched from nodemailer to brevo API mailer due to SMTP limitations in render

--- backend/src/core/config/env.validation.ts
+++ backend/src/core/config/env.validation.ts
@@ -8,11 +8,8 @@ export const enValidationSchema = Joi.object({
     OAUTH_STATE_SECRET: Joi.string().min(32).required(),
     GOOGLE_CLIENT_ID: Joi.string().required(),
     GOOGLE_CLIENT_SECRET: Joi.string().required(),
-    SMTP_FROM: Joi.string().required(),
-    SMTP_HOST: Joi.string().required(),
-    SMTP_PORT: Joi.number().required(),
-    SMTP_USER: Joi.string().required(),
-    SMTP_PASSWORD: Joi.string().required(),
+    BREVO_FROM: Joi.string().required(),
+    BREVO_API_KEY: Joi.string().required(),
     FRONTEND_URL: Joi.string().uri().required(),
     TENANT_DOMAIN_SUFFIX: Joi.string().required(),
 })
--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -1,42 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { PugAdapter } from '@nestjs-modules/mailer/adapters/pug.adapter';
-import { join } from 'path';
-import 'dotenv/config';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [
-    MailerModule.forRootAsync({
-      useFactory: () => ({
-        transport: {
-          host: process.env['SMTP_HOST'],
-          port: 587,
-          secure: false,
-          family: 4,
-          auth: {
-            user: process.env['SMTP_USER'],
-            pass: process.env['SMTP_PASSWORD'],
-          },
-          connectionTimeout: 30000,
-          greetingTimeout: 30000,
-          socketTimeout: 30000,
-          logger: true,
-          debug: true,
-        },
-        defaults: {
-          from: process.env['SMTP_FROM'],
-        },
-        template: {
-          dir: join(process.cwd(), process.env.NODE_ENV === "production" ? 'dist/src/templates' : 'src/templates'),
-          adapter: new PugAdapter(),
-          options: {
-            strict: true,
-          },
-        },
-      }),
-    }),
-  ],
+  imports: [ConfigModule],
   providers: [MailService],
   exports: [MailService],
 })
